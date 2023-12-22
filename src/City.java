7156637febc441447227a461634a417a64edf58b
Piotr Jozwiak <piotrjozwiak23@gmail.com>
2023-12-22 14:28:08 +0100

    distance

---
+++ src/City.java
@@ -0,0 +1,42 @@
+public class City {
+
+    private double x;
+    private double y;
+
+    private String nameOfCity;
+
+
+    public City(double x, double y, String nameOfCity) {
+        this.x = x;
+        this.y = y;
+        this.nameOfCity = nameOfCity;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getNameOfCity() {
+        return nameOfCity;
+    }
+
+    public void setNameOfCity(String nameOfCity) {
+        this.nameOfCity = nameOfCity;
+    }
+
+
+
+
+}
